c3cf0ababcaaac2c9433b72c9b4f3510736d9716
fujicho <nnnn.sohei.fujita@gmail.com>
2020-04-29 10:26:52 +0900

    ローカルコンポーネントの定義

--- practice2/app.js
+++ practice2/app.js
@@ -1,17 +1,26 @@
-Vue.component('fruits-list-title',{
-  template: '<h1>フルーツ一覧</h1>'
+new Vue({
+  el: "#fruits-list",
+  components: {
+    'fruits-list-title': {
+      template: '<h1>フルーツ一覧</h1>'
+    }
+  }
 })
 
-Vue.component('fruits-list',{
-  template: '<div><fruits-list-title></fruits-list-title></div>'
-})
-　
-var FruitsListTitle = Vue.extend({
-  template: '<h1>フルーツ一覧</h1>'
-})
+// Vue.component('fruits-list-title',{
+//   template: '<h1>フルーツ一覧</h1>'
+// })
 
-new FruitsListTitle().$mount('#fruits-list')
+// Vue.component('fruits-list',{
+//   template: '<div><fruits-list-title></fruits-list-title></div>'
+// })
+// 　
+// var FruitsListTitle = Vue.extend({
+//   template: '<h1>フルーツ一覧</h1>'
+// })
 
-var vm = new Vue({
-  el: '#main',
-})
+// new FruitsListTitle().$mount('#fruits-list')
+
+// var vm = new Vue({
+//   el: '#main',
+// })
