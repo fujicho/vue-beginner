edc7a925e01a9d3287556447930aa92b38f4586e
fujicho <nnnn.sohei.fujita@gmail.com>
2020-04-29 17:24:19 +0900

    slot

---
+++ practice4/app.js
@@ -0,0 +1,13 @@
+var headerTemplate = `
+  <div style="color: gray;">
+    <slot name="header>※親から何も渡って来ない場合、この文が表示されます。</slot>
+  </div>
+`
+
+Vue.component('page-header', {
+  template: headerTemplate 
+})
+
+new Vue({
+  el: "#fruits-list"
+})
