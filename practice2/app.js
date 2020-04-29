9a4b1bc836d4ecfebf2f2c12ccc0b3a85e10d810
fujicho <nnnn.sohei.fujita@gmail.com>
2020-04-29 11:53:22 +0900

    コメントアウト

--- practice2/app.js
+++ practice2/app.js
@@ -1,11 +1,31 @@
-new Vue({
-  el: "#fruits-list",
-  components: {
-    'fruits-list-title': {
-      template: '<h1>フルーツ一覧</h1>'
-    }
-  }
-})
+// input type=date のvalueに今日の日付を付与する例
+// Vue.component('input-date-with-today',{
+//   render: function (createElement){
+//     return createElement(
+//       'input',
+//       {
+//         attrs: {
+//           type: 'date',
+//           value: new Date().toISOString().substring(0,10)
+//         }
+//       }
+//     )
+//   }
+// })
+
+// new Vue({el: '#app'})
+
+// Vue.component('fruits-list-title',{
+//   template: '#fruits-list-title'
+// })
+// new Vue({
+//   el: "#fruits-list",
+//   components: {
+//     'fruits-list-title': {
+//       template: '<h1>フルーツ一覧</h1>'
+//     }
+//   }
+// })
 
 // Vue.component('fruits-list-title',{
 //   template: '<h1>フルーツ一覧</h1>'
