cf8774470cec6620e3af8ef68004330c38fe2bf5
fujicho <nnnn.sohei.fujita@gmail.com>
2020-04-29 10:20:14 +0900

    コンストラクタベースの定義

--- practice2/app.js
+++ practice2/app.js
@@ -6,6 +6,12 @@ Vue.component('fruits-list',{
   template: '<div><fruits-list-title></fruits-list-title></div>'
 })
 　
+var FruitsListTitle = Vue.extend({
+  template: '<h1>フルーツ一覧</h1>'
+})
+
+new FruitsListTitle().$mount('#fruits-list')
+
 var vm = new Vue({
   el: '#main',
 })
