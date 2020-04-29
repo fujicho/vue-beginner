e8788161da4d000096b68a4650e655e9d65ee0dc
fujicho <nnnn.sohei.fujita@gmail.com>
2020-04-30 08:34:30 +0900

    スロットのスコープ

--- practice5-slot/app.js
+++ practice5-slot/app.js
@@ -1,13 +1,37 @@
-var MyButton = {
-  template: `
-  <button>
-    <slot>OK</slot>
-  </button>`
-}
+// var MyButton = {
+//   template: `
+//   <button>
+//     <slot>OK</slot>
+//   </button>`
+// }
 
-new Vue({
-  el: '#app',
-  components: {
-    MyButton: MyButton
-  }
-})
+// new Vue({
+//   el: '#app',
+//   components: {
+//     MyButton: MyButton
+//   }
+// })
+
+// var MyPage = {
+//   template: `
+//   <div>
+//     <header>
+//       <slot name="header"></slot>
+//     </header>
+//     <main>
+//     <slot></slot>
+//     </main>
+//     <footer>
+//       <slot name="footer"></slot>
+//     </footer>
+//   </div>
+//   `
+// }
+
+// new Vue({
+//   el: '#app',
+//   components: {
+//     MyPage: MyPage
+//   }
+// })
+
