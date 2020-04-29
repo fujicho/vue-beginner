3a3326af88c6fdf6a96de78919d9239a39cd95a4
fujicho <nnnn.sohei.fujita@gmail.com>
2020-04-29 11:54:43 +0900

    propsの利用

---
+++ practice3/app.js
@@ -0,0 +1,12 @@
+Vue.component('item-desc',{
+  props: {
+    itemName: {
+      type: String
+    }
+  },
+  template: '<p>{{ itemName }}は便利です。</p>'
+})
+new Vue({
+  el: '#app',
+  data: { myItem: 'pen'}
+})
