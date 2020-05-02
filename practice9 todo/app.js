b5ed41dd4825c07444f49137d5ae70990c029d4b
fujicho <nnnn.sohei.fujita@gmail.com>
2020-05-02 10:15:00 +0900

    Update app.js

--- practice9 todo/app.js
+++ practice9 todo/app.js
@@ -1,5 +1,48 @@
+var getUsers = function(callback){
+  setTimeout(function () {
+    callback(null,[
+      {
+        id:1,
+        name: 'Sohei Fujita'
+      },
+      {
+        id:2,
+        name: 'Hamada Takashi'
+      }
+    ])
+  },1000)
+}
+
 var UserList = {
   template: '#user-list',
+  data: function(){
+    return {
+      loading: false,
+      users: function(){ return[] },
+      error: null
+    }
+  },
+
+  created: function(){
+    this.fetchData()
+  },
+
+  watch: {
+    '$route': 'fetchData'
+  },
+  methods: {
+    fetchData: function(){
+      this.loading = true
+      getUsers((function (err, users){
+        this.loading = falase
+        if(err){
+          this.error = err.toString()
+        } else{
+          this.users = users
+        }
+      }).bind(this))
+    }
+  }
 }
 
 var router = new VueRouter({
