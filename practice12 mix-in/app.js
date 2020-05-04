7653711307dc27767a4f8815016d1350f04cea45
fujicho <nnnn.sohei.fujita@gmail.com>
2020-05-04 19:11:08 +0900

    data methodsの記述

--- practice12 mix-in/app.js
+++ practice12 mix-in/app.js
@@ -2,5 +2,24 @@ var IconShareButton = {
   template:`
   <button @click='share'><i class="fas fa-share-square"></i></button>
   `,
-  
+  data: function(){
+    return{
+      _isProcessing: false
+    }
+  },
+  methods: {
+    share: function(){
+      if(this._isProcessing){
+        return
+      }
+      if(!window.confirm("シェアしますか?")){
+        return
+      }
+      this._isProcessing = true
+      setTimeout(() => {
+        window.alert('シェアしました')
+        this._isProcessing = false
+      },300)
+    }
+  }
 }
