a500bbef8a3823b6923d8588d15a931cd908bdca
fujicho <nnnn.sohei.fujita@gmail.com>
2020-05-05 11:44:56 +0900

    Sharableの定義

--- practice12 mix-in/app.js
+++ practice12 mix-in/app.js
@@ -1,3 +1,26 @@
+var Sharable = {
+  data: function(){
+    return {
+      _isProcessing: false
+    }
+  },
+  methods: {
+    share: function(){
+      if(this._isProcessing){
+        return
+      }
+      if(!window.confirm("シェアしますか?")){
+        return
+      }
+      this._isProcessing = true
+      setTimeout(() => {
+        window.alert('シェアしました')
+        this._isProcessing = false
+      },300)
+    }
+  }
+}
+
 var IconShareButton = {
   template:`
   <button @click='share'><i class="fas fa-share-square"></i></button>
