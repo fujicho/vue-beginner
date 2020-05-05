d52233be48f87b470baee4f0142408cf9ad41786
fujicho <nnnn.sohei.fujita@gmail.com>
2020-05-05 17:19:19 +0900

    単一ファイルコンポーネントのpractice

---
+++ practice13/hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <p class="message">
+    {{ msg }}
+  </p>
+</template>
+
+<script>
+export default {
+  data(){
+    return { msg: 'こんにちは'}
+  }
+}
+</script>
+
+<style>
+  .message {color: #42b983}
+</style>
