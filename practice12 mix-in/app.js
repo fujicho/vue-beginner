8cc393c2d0fa46662674cfdca36759ee168abaaa
fujicho <nnnn.sohei.fujita@gmail.com>
2020-05-05 11:52:52 +0900

    リファクタリング

--- practice12 mix-in/app.js
+++ practice12 mix-in/app.js
@@ -22,53 +22,20 @@ var Sharable = {
 }
 
 var IconShareButton = {
+  mixins: [Sharable],
   template:`
   <button @click='share'><i class="fas fa-share-square"></i></button>
-  `,
-  data: function(){
-    return{
-      _isProcessing: false
-    }
-  },
-  methods: {
-    share: function(){
-      if(this._isProcessing){
-        return
-      }
-      if(!window.confirm("シェアしますか?")){
-        return
-      }
-      this._isProcessing = true
-      setTimeout(() => {
-        window.alert('シェアしました')
-        this._isProcessing = false
-      },300)
-    }
-  }
+  `
 }
 
 var TextShareButton = {
+  mixins: [Sharable],
   template:`
   <button @click="share">{{ buttonLabel }}</button>
   `,
   data: function(){
     return{
-      buttonLabel: 'シェアする',
-      _isProcessing: false
-    }
-  },
-  methods: {
-    share: function(){
-      if (this._isProcessing){
-        return
-      }
-      if (!window.confirm('シェアしますか？')){
-        return
-      }
-      this._isProcessing = true
-      setTimeout(() => {
-        window.alert('シェアしました')
-      },300)
+      buttonLabel: 'シェアする'
     }
   }
 }
